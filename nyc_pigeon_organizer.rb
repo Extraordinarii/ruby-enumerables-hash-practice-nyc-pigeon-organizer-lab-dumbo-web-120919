e148779a5f5738e3ee2af85a35b7a8ac3ada6bf4
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-04 00:22:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
+  pigeon_data = {} #establishing a hash that
+  data.each do |types, value| #we go through the types of pigeons 
+  value.each do |differences, name|# then we go through the differences between the pigeons 
+    name.each do |name| #iterate through the name & key
+      if !pigeon_data[name] #create a pigeon value if there isn't any present  
+        pigeon_data[name] = {} #we add it to the hash 
+      end 
+      if !pigeon_data[name][types] #create a attribute
+        pigeon_data[name][types] = [] #we add it to the array 
+      end 
+      pigeon_data[name][types] << differences.to_s #we place the type inside it as a string  
+    end 
+  end 
+end
+return pigeon_data
+  #will hold our pigeon information 
   # write your code here!
 end
